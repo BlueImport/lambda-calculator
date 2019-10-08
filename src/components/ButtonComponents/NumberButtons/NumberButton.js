1ffb7b6211b8da923e174914d5ef9069830386e0
Kris <kristian.claire.101@gmail.com>
2019-10-08 13:59:07 -0700

    adds numbers and special char to calc

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const NumberButton = (props) => {
+const NumberButton = props => {
   return (
     <button className = "button number">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
